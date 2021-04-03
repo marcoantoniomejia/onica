c485b560edbd6550d89b1d9f442dc0f8cd7a99e7
Marco Mejia <32719760+marcoantoniomejia@users.noreply.github.com>
2021-04-02 18:54:25 -0600

    primer

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+// Muestra toda la información, por defecto INFO_ALL
+//phpinfo();
+echo "hola mundo\p"
+echo "Este es el hostname "
+echo gethostname();
+// Muestra solamente la información de los módulos.
+// phpinfo(8) hace exactamente lo mismo.
+//phpinfo(INFO_MODULES);
+
+?>
+
